e3719cb6e270652220cc838146a2954182545d86
CF12 <brianx0011@gmail.com>
2017-05-19 23:16:29 -0400

    Fixed a few alignment issues, and began working on footer

--- src/PageMain.jsx
+++ src/PageMain.jsx
@@ -16,13 +16,28 @@ class PageMain extends Component {
 
         <div className="wrapper-body">
           <div className="body-main-1">
-            <div className="left">
-              <p className="main-title"> About Me </p>
-              <p className="main-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque metus massa, porttitor id ex quis, ullamcorper mollis neque. Aenean bibendum venenatis massa. Pellentesque et sodales ex. Nam varius ligula et ante suscipit, non posuere libero elementum. Maecenas ac justo sem. Vivamus euismod in risus vel dapibus. Nullam et urna eget tortor scelerisque feugiat ut et urna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin aliquet tempus magna laoreet sagittis. Maecenas vulputate erat orci, ac euismod elit suscipit at. In consectetur laoreet turpis vitae maximus. Pellentesque finibus sed ipsum sed hendrerit. Morbi urna tellus, bibendum nec odio et, dictum vulputate nibh. </p>
+            <div className="left-pane">
+              <div className="text-wrapper">
+                <p className="main-title"> About Me </p>
+                <p className="main-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque metus massa, porttitor id ex quis, ullamcorper mollis neque. Aenean bibendum venenatis massa. Pellentesque et sodales ex. Nam varius ligula et ante suscipit, non posuere libero elementum. Maecenas ac justo sem. Vivamus euismod in risus vel dapibus. Nullam et urna eget tortor scelerisque feugiat ut et urna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin aliquet tempus magna laoreet sagittis. Maecenas vulputate erat orci, ac euismod elit suscipit at. In consectetur laoreet turpis vitae maximus. Pellentesque finibus sed ipsum sed hendrerit. Morbi urna tellus, bibendum nec odio et, dictum vulputate nibh. </p>
+              </div>
             </div>
 
-            <div className="right">
-              <img className="main-image" src={Bg1} />
+            <div className="right-pane">
+              <img className="main-image" src={Bg1} alt="Not found." />
+            </div>
+          </div>
+
+          <div className="body-main-2">
+            <div className="left-pane">
+              <img className="main-image" src={Bg1} alt="Not found." />
+            </div>
+
+            <div className="right-pane">
+              <div className="text-wrapper">
+                <p className="main-title"> About Me </p>
+                <p className="main-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque metus massa, porttitor id ex quis, ullamcorper mollis neque. Aenean bibendum venenatis massa. Pellentesque et sodales ex. Nam varius ligula et ante suscipit, non posuere libero elementum. Maecenas ac justo sem. Vivamus euismod in risus vel dapibus. Nullam et urna eget tortor scelerisque feugiat ut et urna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin aliquet tempus magna laoreet sagittis. Maecenas vulputate erat orci, ac euismod elit suscipit at. In consectetur laoreet turpis vitae maximus. Pellentesque finibus sed ipsum sed hendrerit. Morbi urna tellus, bibendum nec odio et, dictum vulputate nibh. </p>
+              </div>
             </div>
           </div>
         </div>
