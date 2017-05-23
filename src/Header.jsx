992953d2a7e38b826bb254754c767437dbf3e9cc
CF12 <brianx0011@gmail.com>
2017-05-22 22:57:39 -0400

    Work on the content of the main page, especially the "pros and cons" section.

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,19 +9,17 @@ class Header extends Component {
 
   render () {
     return (
-      <div>
-        <span className="wrapper-header">
-          <div className="button button-main">
-            <Link to="/" className="no-nav-style"> CF12 </Link>
-          </div>
-          <div className="button button-about no-nav-style">
-            <Link to="/about" className="no-nav-style"> ABOUT </Link>
-          </div>
-          <div className="button button-projects no-nav-style">
-            <Link to="/projects" className="no-nav-style"> PROJECTS </Link>
-          </div>
-        </span>
-      </div>
+      <span className="wrapper-header">
+        <div className="button button-main">
+          <Link to="/" className="no-nav-style"> CF12 </Link>
+        </div>
+        <div className="button button-about no-nav-style">
+          <Link to="/about" className="no-nav-style"> ABOUT </Link>
+        </div>
+        <div className="button button-projects no-nav-style">
+          <Link to="/projects" className="no-nav-style"> PROJECTS </Link>
+        </div>
+      </span>
     )
   }
 }
