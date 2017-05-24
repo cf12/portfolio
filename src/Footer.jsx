58d9d48a76d5311c57b57d66b40f81cfb824cd64
CF12 <brianx0011@gmail.com>
2017-05-23 23:01:40 -0400

    Work on finishing pro cons, and begining work on projects panel

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 import './Footer.css'
 
 class Footer extends Component {
+  constructor (top) {
+    super()
+
+    this.top = top
+  }
+
   render () {
     return (
       <span className="wrapper-footer">
