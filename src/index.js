d0e448029ca1d0e732b4c0a19e8790ab4fdbb1a9
CF12 <brianx0011@gmail.com>
2019-10-28 23:38:34 -0400

    Remove github pages & switch back to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import 'normalize.css'
 import './styles/global.scss'
