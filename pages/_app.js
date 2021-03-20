1bff647e24ae868082b88d57bb1560eeea3ec9e8
CF12 <brianx0011@gmail.com>
2021-03-20 00:29:55 -0400

    Remove console.log

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,6 @@ import NextNprogress from 'nextjs-progressbar'
 import colors from 'theme/_colors.module.scss'
 import 'theme/global.scss'
 
-console.log(colors)
-
 function MyApp ({ Component, pageProps }) {
   return (
     <>
